659b10547544ae82ccd6024468255387bd0dde49
damiryan24 <damiryan24@mail.ru>
2025-02-18 18:51:54 +0400

    Update attempt_client.py

--- attempt_client.py
+++ attempt_client.py
@@ -9,27 +9,30 @@ client.settimeout(5)
 
 while True:
     print('new cycle ("continue" to begin, "end" to stop)')
-    command = input()                                               #continue
-    client.sendall(str.encode(command, 'WINDOWS-1251'))
-    command = input('domain:')                                               #domain
-    filename = str(command + '.xlsx')
-    client.sendall(str.encode(command, 'WINDOWS-1251'))
-    command = input('count:')                                               #count
-    client.sendall(str.encode(command, 'WINDOWS-1251'))
-    command = input('offset:')                                               #offset
-    client.sendall(str.encode(command, 'WINDOWS-1251'))
-    with open(filename, 'wb') as f:
-        while True:
-            try:
-                bytes_read = client.recv(1024)
-                f.write(bytes_read)
-                bytes_read = None
-            except:
-                break
-                '''try:
-                    if bytes_read.decode('WINDOWS-1251') == 'конец файла':
-                        print('end')
-                        break
+    command = input()
+    if command == "continue":      #continue
+        client.sendall(str.encode(command, 'WINDOWS-1251'))
+        command = input('domain:')                                               #domain
+        filename = str(command + '.xlsx')
+        client.sendall(str.encode(command, 'WINDOWS-1251'))
+        command = input('count:')                                               #count
+        client.sendall(str.encode(command, 'WINDOWS-1251'))
+        command = input('offset:')                                               #offset
+        client.sendall(str.encode(command, 'WINDOWS-1251'))
+        with open(filename, 'wb') as f:
+            while True:
+                try:
+                    bytes_read = client.recv(1024)
+                    f.write(bytes_read)
+                    bytes_read = None
                 except:
-                    pass'''
-    print('file received')
+                    break
+                    '''try:
+                        if bytes_read.decode('WINDOWS-1251') == 'конец файла':
+                            print('end')
+                            break
+                    except:
+                        pass'''
+        print('file received')
+    else:
+        break
